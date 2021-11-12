8fb15ecf27cc272b167297ae73f07706ea62a131
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-11-12 16:45:19 -0500

    formSchema laid out

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,1 +1,27 @@
- 
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+   name: yup
+       .string()
+       .trim()
+       .required('name is a required field')
+       .min(2, 'name must be at least 2 characters'),
+   size: yup
+       .string()
+       .oneOf(['Small', 'Medium', 'Large', 'X-Large'])
+       .required('size is a required field'),
+   special: yup
+       .string(),
+   mushroom: yup
+       .boolean(),
+   olive: yup
+      .boolean(),
+   pineapple: yup
+      .boolean(),
+   banana: yup
+      .boolean()
+
+
+})
+
+export default formSchema
