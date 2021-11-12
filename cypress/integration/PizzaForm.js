6e58ace1e487b0ebc67f61d522b390e368d99e07
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-11-12 13:06:59 -0500

    Cypress Implemented - Basic Tests Running

---
+++ cypress/integration/PizzaForm.js
@@ -0,0 +1,30 @@
+describe('Form', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/pizza');
+    })
+    const nameInput = () => cy.get('input[name=name]');
+    const banana = () => cy.get('input[name=banana]');
+    const mushroom = () => cy.get('input[name=mushroom]');
+    const olive = () => cy.get('input[name=olive]');
+    const pineapple = () => cy.get('input[name=pineapple]')
+    const foobarInput = () => cy.get('input[name=foobar]');
+    const specialText = () => cy.get('textarea');
+
+    it('sanity check', () => {
+        expect(1+2).to.equal(3);
+        expect(2+2).not.to.equal(11);
+        expect({}).not.to.equal({});
+    })
+
+    it('elements rendering to screen', () => {
+        nameInput().should('exist');
+        foobarInput().should('not.exist');
+        banana().should('exist');
+        mushroom().should('exist');
+        olive().should('exist');
+        pineapple().should('exist');
+        specialText().should('exist');
+        cy.contains('Place Order!').should('exist');
+
+    })
+})
